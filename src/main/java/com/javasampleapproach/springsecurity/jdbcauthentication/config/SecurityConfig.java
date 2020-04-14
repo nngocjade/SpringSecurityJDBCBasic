9ea5988193abc67d115fa1c0719da4a6ff4eda41
Ryan Desmond <radesmond@gmail.com>
2020-04-14 11:07:41 -0700

    pushing template spring security - has multi-tier auth bug still

--- src/main/java/com/javasampleapproach/springsecurity/jdbcauthentication/config/SecurityConfig.java
+++ src/main/java/com/javasampleapproach/springsecurity/jdbcauthentication/config/SecurityConfig.java
@@ -7,10 +7,12 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
  
 @Configuration
 @EnableAutoConfiguration
+@EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
  
 	@Autowired
@@ -27,7 +29,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests()
 				.antMatchers("/", "/home").permitAll()
-				.antMatchers("/admin").hasRole("ADMIN")
 				.anyRequest().authenticated()
 				.and().formLogin().loginPage("/login").permitAll()
 				.and().logout().permitAll();
--- src/main/java/com/javasampleapproach/springsecurity/jdbcauthentication/controller/WebController.java
+++ src/main/java/com/javasampleapproach/springsecurity/jdbcauthentication/controller/WebController.java
@@ -1,8 +1,8 @@
 package com.javasampleapproach.springsecurity.jdbcauthentication.controller;
 
-import com.javasampleapproach.springsecurity.jdbcauthentication.Services.UserService;
-import com.javasampleapproach.springsecurity.jdbcauthentication.model.User;
-import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,25 +12,23 @@ import java.util.ArrayList;
 @Controller
 public class WebController {
 
-    @Autowired
-    UserService userService;
    
     @RequestMapping(value="/")
     public String home(){
+
         return "home";
     }
    
     @RequestMapping(value="/user")
-    public String user(){
+    public String user(Authentication authentication){
+        UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+        System.out.println("User has authorities: " + userDetails.getAuthorities());
+
         return "user";
     }
   
     @RequestMapping(value="/admin")
     public String admin(Model model){
-
-        ArrayList<User> users = userService.getUsersAndTheirAnimals();
-
-        model.addAttribute("users", users);
 
         return "admin";
 
@@ -38,15 +36,13 @@ public class WebController {
    
     @RequestMapping(value="/login")
     public String login(){
+
         return "login";
     }
    
     @RequestMapping(value="/403")
     public String Error403(){
-        return "403";
-    }
 
-    public Object getDateAndTime() {
-        return null;
+        return "403";
     }
 }
